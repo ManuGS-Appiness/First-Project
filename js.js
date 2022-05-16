c4829f458447c269abe508e4a68abd94fdc27de2
Manu G S <manu.gs@appinessworld.com>
2022-05-16 15:33:04 +0530

    NEW UPDATE

--- js.js
+++ js.js
@@ -1,6 +1,4 @@
-const first=document.querySelector("#FirName");
-const number=document.querySelector("#Number");
-const second=document.querySelector("#SecName");
+
 // first.onclick=function(){
 //     alert("OHH NO");
 // }
@@ -9,7 +7,7 @@ const second=document.querySelector("#SecName");
 //     for(let i in second){
 //         if (type(first)===string)
 //         {
-            
+
 //         }
 //     }
 //     alert("OHH YES");
@@ -30,23 +28,46 @@ const second=document.querySelector("#SecName");
 
 
 
-const bookData = [];
+// const bookData = [];
 
 
-function getAppointmentData(e ){
-e.preventDefault()
-const FirstName = document.getElementById("FirName")
-const SecondName = document.getElementById("SecName")
-const email = document.getElementById("Email")
-const number = document.getElementById("number")
-const password=document.getElementById("Passwordss")
+// function getAppointmentData(e ){
+// e.preventDefault()
+// const FirstName = document.getElementById("FirName")
+// const SecondName = document.getElementById("SecName")
+// const email = document.getElementById("Email")
+// const number = document.getElementById("number")
+// const password=document.getElementById("Passwordss")
 
-const data = { name: FirstName.value,SecondName:SecondName.value, email: email.value, number: number.value,password:password.value
+// let FirstName=[]
+// let SecondName=[]
+// let email=[]
+// let number=[]
+// let password=[]
 
-}
-bookData.push(data)
+// const data = { name: FirstName.value,SecondName:SecondName.value, email: email.value, number: number.value,password:password.value
+
+// }
+// bookData.push(data)
 // console.log(name.value, email.value,number.value);
 
-console.log(bookData)
-}
+// console.log(bookData)
+// }
+const manubtn = document.querySelector("#navbt1");
+const first = document.querySelector("#FirName");
+const number = document.querySelector("#Number");
+const second = document.querySelector("#SecName");
+const email = document.querySelector('#Email');
+const password = document.querySelector("Passwordss");
+
+manubtn.addEventListener('click', function () {
+    if ((first.value.length != "") && ((second.value.length != "") || (second.value.length == "")) && (number.value.length == 10) && (email.value.length != "") && ((password.value.length >= 8) && (password.value.length <= 13)));
+    {
+        alert("Welcome to Maanto, You are a now a subscriber");
+    }
+
+
+        alert("Fill the section correctly");
+   
+})
 
